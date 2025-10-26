73279ef15737635a486c3827416e09652c8c1a20
Kevin Ying <yingk@berkeley.edu>
2025-10-25 21:51:33 -0700

    added string fixes

--- Yokombinator_SWIFT/Yokombinator_SWIFT/main.swift
+++ Yokombinator_SWIFT/Yokombinator_SWIFT/main.swift
@@ -2,7 +2,7 @@ let claude = OpenRouterManager(apiKey: "sk-or-v1-435b9442595200eab7d9bbbce321b33
 
 Task {
     let response = try await claude.sendMessage(
-        "Suppose you are a squirrel. Give a quick, one-sentence reaction to this image. Give a squirrel happiness percentage. Your response should be in the form:" + "\n" + "\n" + "[reaction text (string)] + "\n" + "\n" + [squirrel happiness percentage]", 
+        "Suppose you are a squirrel. Give a quick, one-sentence reaction to this image. Give a squirrel happiness percentage. Your response should be in the form:" + "\n" + "\n" + "[reaction text (string)] + ",\n," + ",\n," + [squirrel happiness percentage]",
         imagePaths: ["images/test_img1.jpg"]
     )
     print(response)
