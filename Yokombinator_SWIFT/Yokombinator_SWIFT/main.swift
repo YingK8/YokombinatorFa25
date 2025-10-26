ae34a0d10e8c0db7701eec6c1f69e56d421c3985
Owen Lam <owen.lam@berkeley.edu>
2025-10-25 21:29:53 -0700

    openrouter stuff + main + images folder

---
+++ Yokombinator_SWIFT/Yokombinator_SWIFT/main.swift
@@ -0,0 +1,9 @@
+let claude = OpenRouterManager(apiKey: "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")
+
+Task {
+    let response = try await claude.sendMessage(
+        "Suppose you are a squirrel. Give a quick, one-sentence reaction to this image. Give a squirrel happiness percentage. Your response should be in the form:" + "\n" + "\n" + "[reaction text (string)] + "\n" + "\n" + [squirrel happiness percentage]", 
+        imagePaths: ["images/test_img1.jpg"]
+    )
+    print(response)
+}
