be94c594b87fdc7676314b138442147466550b5c
Kevin Ying <yingk@berkeley.edu>
2025-10-26 09:20:22 -0700

    merged :)

--- Yokombinator_SWIFT/Yokombinator_SWIFT/AI/SquirrelHandler.swift
+++ Yokombinator_SWIFT/Yokombinator_SWIFT/AI/SquirrelHandler.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 public class SquirrelHandler {
-    private let apiKey: String
-    private let OpenRouter: OpenRouterManager
+    private var apiKey: String
+    private var OpenRouter: OpenRouterManager
     private let setup: bool = false
     private var
     
@@ -14,15 +14,15 @@ public class SquirrelHandler {
             self.OpenRouter = OpenRouter
     }
     
-    public setapiKey(apiKey: String) {
+    public func setapiKey(apiKey: String) {
         self.apiKey = apiKey
-        self.OpenRouter = OpenRouterManager(apiKey = self.apiKey)
+        self.OpenRouter = OpenRouterManager(apiKey: apiKey = self.apiKey)
     }
     
-    public getImageResponse(base64ImgURL: String) {
+    public func getImageResponse(base64ImgURL: String) -> String {
         return try await OpenRouter.sendMessage(
             "You are a squirrel. Give a short, direct, one-sentence (less than 15 words) reaction to this image. Give a squirrel happiness level. Your response should be in the form:" + "\n" + "\n" + "\"<reaction text (string)>" + "\n" + "\n" + "<squirrel happiness level (float in [0, 1])>\"",
-            imagePaths: [], base64ImgURLs = [base64ImgURL]
+            imagePaths: [], base64ImgURLs: base64ImgURLs: [base64ImgURL]
         )
     }
     
